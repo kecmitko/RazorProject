22fa1ff3e8fe5bb84fec92b6db0b03672ed52c4d
Mitko Keckaroski <kecmitko@gmail.com>
2020-09-18 11:20:44 +0200

    added tag helper

---
+++ WebApplication10/TagHelpers/MyFirstTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApplication10.TagHelpers
+{
+    [HtmlTargetElement("my-first-tag-helper")]
+    public class MyFirstTagHelper : TagHelper
+    {
+        public string Header { get; set; }
+        public string Text { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            var sb = new StringBuilder();
+            sb.AppendFormat("<div style='border: 1px solid {0}'>", "red");
+            sb.AppendFormat("<h1>{0}</h1>", this.Header);
+            sb.AppendFormat("<p>This is some text for the banner! {0}</h1>", this.Text);
+            sb.AppendFormat("<p>Active till {0} </p>", DateTime.Now);
+            sb.AppendFormat("</div>");
+
+            output.PreContent.SetHtmlContent(sb.ToString());
+        }
+    }
+}
