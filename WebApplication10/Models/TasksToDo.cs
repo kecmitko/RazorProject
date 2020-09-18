6e9850e855e64e805592020bcc1a654f4b374bd5
Mitko Keckaroski <kecmitko@gmail.com>
2020-09-18 18:03:14 +0200

    commit

--- WebApplication10/Models/TasksToDo.cs
+++ WebApplication10/Models/TasksToDo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace WebApplication10.Models
     public class TasksToDo
     {
         public int RedenBroj { get; set; }
+        [Required]
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
